f5b92b9a3429808d409c169db4a173ee1f1b5a0b
moenguyenx <qminh1908@gmail.com>
2024-05-22 10:09:02 +0700

    temporary comment refresh jwt func since updated session to 12h

--- backend/ChatBot/routes.py
+++ backend/ChatBot/routes.py
@@ -11,25 +11,25 @@ from flask_jwt_extended import create_access_token,get_jwt,get_jwt_identity, \
 Authentication Reference: 
 "https://dev.to/nagatodev/how-to-add-login-authentication-to-a-flask-and-react-application-23i7"
 '''
-@api.after_request
-def refresh_expiring_jwts(response):
-    """
-    Refresh expiring JWT after 30mins
-    """
-    try:
-        exp_timestamp = get_jwt()["exp"]
-        now = datetime.now(timezone.utc)
-        target_timestamp = datetime.timestamp(now + timedelta(minutes=30))
-        if target_timestamp > exp_timestamp:
-            access_token = create_access_token(identity=get_jwt_identity())
-            data = response.get_json()
-            if type(data) is dict:
-                data["access_token"] = access_token
-                response.data = json.dumps(data)
-        return response
-    except (RuntimeError, KeyError):
-        # Case where there is not a valid JWT. Just return the original response
-        return response
+# @api.after_request
+# def refresh_expiring_jwts(response):
+#     """
+#     Refresh expiring JWT after 30mins
+#     """
+#     try:
+#         exp_timestamp = get_jwt()["exp"]
+#         now = datetime.now(timezone.utc)
+#         target_timestamp = datetime.timestamp(now + timedelta(minutes=30))
+#         if target_timestamp > exp_timestamp:
+#             access_token = create_access_token(identity=get_jwt_identity())
+#             data = response.get_json()
+#             if type(data) is dict:
+#                 data["access_token"] = access_token
+#                 response.data = json.dumps(data)
+#         return response
+#     except (RuntimeError, KeyError):
+#         # Case where there is not a valid JWT. Just return the original response
+#         return response
 
 
 @api.route('/token', methods=["POST"])
